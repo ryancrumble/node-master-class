c00c0d28b164834db5362f6f80a14a96a62f393f
ryancrumble <ryan.crummy@pm.me>
2022-12-05 15:26:31 +1100

    feat: access an API without a library

--- app/lib/helpers.js
+++ app/lib/helpers.js
@@ -2,11 +2,11 @@
  * Helpers for various tasks
  */
 
-const crypto = require('crypto');
-const config = require('./config');
+const crypto = require('crypto')
+const config = require('./config')
+const querystring = require('querystring')
+const https = require('https')
 
-// Container for helpers
-const helpers = {};
 
 /**
  * @name HashHelper
@@ -14,13 +14,13 @@ const helpers = {};
  * @param rawString {string}
  * @return {string|boolean}
  */
-helpers.hash = function (rawString) {
-    if (typeof (rawString) !== 'string' || rawString.length <= 0) {
-        return false;
-    }
+const hash = function(rawString) {
+  if (typeof (rawString) !== 'string' || rawString.length <= 0) {
+    return false
+  }
 
-    return crypto.createHmac('sha256', config.hashingSecret).update(rawString).digest('hex');
-};
+  return crypto.createHmac('sha256', config.hashingSecret).update(rawString).digest('hex')
+}
 
 /**
  * @name ParseJSONToObject
@@ -28,13 +28,13 @@ helpers.hash = function (rawString) {
  * @param string {string}
  * @return any | {} | undefined
  */
-helpers.parseJsonToObject = function (string) {
-    try {
-        return JSON.parse(string);
-    } catch (e) {
-        return {};
-    }
-};
+const parseJsonToObject = function(string) {
+  try {
+    return JSON.parse(string)
+  } catch (e) {
+    return {}
+  }
+}
 
 
 /**
@@ -42,20 +42,87 @@ helpers.parseJsonToObject = function (string) {
  * @desc Generate a random string
  * @param stringLength {number}
  */
-helpers.createRandomString = function (stringLength) {
-    if (!stringLength) {
-        return false;
+const createRandomString = function(stringLength) {
+  if (!stringLength) {
+    return false
+  }
+
+  // Declare all characters
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  let str = ''
+  for (let i = 0; i < stringLength; i++) {
+    str += chars.charAt(Math.floor(Math.random() * chars.length))
+  }
+
+  return str
+}
+
+/**
+ *
+ */
+const sendTwilioSms = function(phone, message, callback) {
+  // Validate params
+  if (typeof phone !== 'string' || phone.trim().length !== 9) {
+    return callback('Phone number is invalid. Please check the phone number is provided and the length of the number')
+  }
+
+  if (typeof message !== 'string' || message.trim().length <= 0 || message.trim().length > 1600) {
+    return callback('Message is invalid. Please check the message is provided and within the valid length')
+  }
+
+  // Configure the API request payload
+  const payload = {
+    'From': config.twilio.fromPhone,
+    'To': '+61' + phone,
+    'Body': message,
+  }
+
+  // Stringify payload (from 'querystring' module, not JSON.stringify())
+  // because the Content-Type is not application/JSON.
+  const stringPayload = querystring.stringify(payload)
+
+
+  // Configure the request details
+  const requestDetails = {
+    protocol: 'https:',
+    hostname: 'api.twilio.com',
+    method: 'POST',
+    path: '/2010-04-01/Accounts/' + config.twilio.accountSid + '/Messages.json',
+    auth: config.twilio.accountSid + ':' + config.twilio.authToken,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(stringPayload),
+    },
+  }
+
+  // Instantiate the request object
+  const req = https.request(requestDetails, (res) => {
+    // Grab the status of the send request
+    const status = res.statusCode
+
+    // Callback depending on statusCod
+    if (status !== 200 || status !== 201) {
+      return callback(status + ' ' + res.statusMessage)
     }
 
-    // Declare all characters
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    return callback(false)
+  })
 
-    let str = '';
-    for (let i = 0; i < stringLength; i++) {
-        str += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
 
-    return str;
-};
+  // Bind to the error event to NOT throw an error
+  req.on('error', (e) => callback(e))
 
-module.exports = helpers;
+  // Add the payload
+  req.write(stringPayload)
+
+  // End the request
+  req.end()
+}
+
+module.exports = {
+  hash,
+  parseJsonToObject,
+  createRandomString,
+  sendTwilioSms,
+}
